d9f89633b39670da3d3f5194e6fdd741c290855b
dkilfoyle <dkilfoyle@xtra.co.nz>
2017-06-29 21:59:10 +1200

    Start version using quasar webpack

---
+++ src/components/Index.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        North/Midlands Acute Stroke Clot Retrieval Screening Tool
+      </q-toolbar-title>
+    </div>
+
+    <div class="layout-view">
+      <div class="layout-padding">
+
+        .card.bg-teal.text-white
+          .card-title Info
+          .card-content 
+            p A screening tool to identify acute stroke patients potentially suitable for urgent transfer to Auckland Hospital for endovascular clot retrieval
+
+        <onset-time></onset-time>
+
+        <patient-criteria></patient-criteria>
+
+        </div>
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import OnsetTime from './OnsetTime.vue'
+import PatientCriteria from './PatientCriteria.vue'
+
+export default {
+  components: {
+    OnsetTime,
+    PatientCriteria
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ src/components/PatientCriteria.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+.card
+  .card-title.bg-primary.text-white Patient Criteria
+  .card-content
+
+    .list
+      label.item
+        .item-primary
+          q-checkbox(v-model="age")
+        .item-content
+          |Is the patient >= 15y old?
+      label.item
+        .item-primary
+          q-checkbox(v-model="functional")
+        .item-content
+          |Is the baseline functional level at least semi-independent?
+      label.item
+        .item-primary
+          q-checkbox(v-model="BSL")
+        .item-content
+          |Is the BSL between 4-17 mmol/L
+      label.item
+        .item-primary
+          q-checkbox(v-model="hospital")
+        .item-content
+          |Is the patient currently in a Midlands or Northern hospital?
+
+    //- .column.group
+    //-   label(style="padding:50px")
+
+        
+    //-   label
+    //-     q-checkbox(v-model="functional")
+    //-     
+    //-   label
+    //-     q-checkbox(v-model="BSL")
+    //-     |   
+    //-   label
+    //-     q-checkbox(v-model="hospital")
+    //-     |   
+
+</label>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      age: false,
+      functional: false,
+      BSL: false,
+      hospital: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
